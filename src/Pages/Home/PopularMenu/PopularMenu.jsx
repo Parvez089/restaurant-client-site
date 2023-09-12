dd637db11023171e49fda15a0e040ebfae83f2e3
Mosharof Hossen Parvez <mhparvez089@gmail.com>
2023-09-12 10:33:52 +0600

    add items

---
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,15 @@
+/** @format */
+
+import SectionTitle from "../../../Components/SectionTitle";
+
+const PopularMenu = () => {
+  return (
+    <section>
+      <SectionTitle
+        subHeading={"Popular Items"}
+        heading='From Our Menu'></SectionTitle>
+    </section>
+  );
+};
+
+export default PopularMenu;
